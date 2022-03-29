f7ff1e834dbeb33cd61b6bd3708b07398c946f64
wwkang94 <wwkang94@naver.com>
2022-03-29 17:18:15 +0900

    vue router

--- part1/ch02/src/main.js
+++ part1/ch02/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+// 1. Define route components.
+import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './pages/LoginPage.vue'
+
+// 2. Define some routes
+const routes = [
+  { path: "/", component: HelloWorld },
+  { path: "/login", component: LoginPage }
+];
+
+// 3. Create the router instance and pass the `routes` option
+const router = new VueRouter({
+  routes
+});
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
