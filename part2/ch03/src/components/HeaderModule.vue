114980bfa001114a9011a45fc89a5ce3dcae7b49
wwkang94 <wwkang94@naver.com>
2022-03-30 13:16:58 +0900

    ✨ Profile page #7

---
+++ part2/ch03/src/components/HeaderModule.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    <li>
+      <router-link to="/">Main</router-link>
+    </li>
+    <li>
+      <router-link to="/login">Login</router-link>
+    </li>
+    <li>
+      <router-link to="/profile">Profile</router-link>
+    </li>
+  </ul>
+</template>
--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import MainPage from './pages/MainPage.vue'
+import LoginPage from './pages/LoginPage.vue'
+import ProfilePage from './pages/ProfilePage.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 
-const routes = [{
-  path: '/', component: MainPage
-}];
+const routes = [
+  { path: '/', component: MainPage },
+  { path: '/login', component: LoginPage },
+  { path: '/profile', component: ProfilePage }
+];
 
 const router = new VueRouter({
   routes
