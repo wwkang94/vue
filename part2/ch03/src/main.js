5fd1ce59abaf4492849ff770b76e5ffa96acc9c7
wwkang94 <wwkang94@naver.com>
2022-03-30 11:24:59 +0900

    ✨ Main Page vue-router #5

--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import MainPage from './pages/MainPage.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [{
+  path: '/', component: MainPage
+}];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
