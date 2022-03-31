98f924d4d3abcf59cf09e49f8bfc892ff188b125
wwkang94 <wwkang94@naver.com>
2022-03-31 15:56:15 +0900

    update

--- part3/vue3/src/App.vue
+++ part3/vue3/src/App.vue
@@ -5,8 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo_star.png" />
-  <HelloWorld msg="Hello!!" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
